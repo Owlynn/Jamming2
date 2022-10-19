795d5e5b25de1454ff6810aa620571c753d04273
Owlynn <contact@jessalynn.fr>
2022-10-19 11:50:48 +0200

    Passing props between elements

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,20 +2,34 @@ import './App.css';
 import SearchBar from '../../Components/SearchBar/SearchBar.js'
 import SearchResults from '../../Components/SearchResults/SearchResults.js'
 import PlayList from '../../Components/PlayList/PlayList.js'
+import React from 'react';
 
-function App() {
-  return (
-    <div>
-  <h1>Ja<span className="highlight">mmm</span>ing</h1>
-  <div className="App">
-    <SearchBar/>
-    <div className="App-playlist">
-      <SearchResults/>
-      <PlayList/>
-    </div>
-  </div>
-</div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state= {
+      searchResults : [
+        {name:'Nom', artist : 'Artiste',album : 'Album',id : '123'},
+        {name:'Nom2', artist : 'Artiste2',album : 'Album2',id : '1232'},
+        {name:'Nom3', artist : 'Artiste3',album : 'Album3',id : '1233'}
+      ]
+    }
+  }
+
+  render(){
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar/>
+          <div className="App-playlist">
+            <SearchResults searchResults = {this.state.searchResults}/>
+            <PlayList/>
+          </div>
+        </div>
+      </div>
+    );
+  } 
 }
 
 export default App;
--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -23,15 +23,13 @@ class Track extends React.Component{
             )
             
         }
-
-
     }
     render(){
         return (
             <div class="Track">
                 <div class="Track-information">
-                    <h3>TrackName</h3>
-                    <p>Track Artist |TrackAlbum</p>
+                    <h3>{this.props.track.name}</h3>
+                    <p>{this.props.track.artist} | {this.props.track.album}</p>
                 </div>
                 {this.renderAction()}
             </div>
